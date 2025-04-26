f9b6a6e3012a7d78b771eafc46b32083414a48ff
soikat-san <soikat.codes@gmail.com>
2025-04-26 20:05:15 +0530

    shared dependencies

---
+++ products/src/bootstrap.js
@@ -0,0 +1,22 @@
+import faker from "faker";
+
+const mount = (el) => {
+  let products = "";
+
+  for (let i = 0; i < 10; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  if (el) {
+    mount(el); // when in isolation
+  }
+}
+
+export { mount };
